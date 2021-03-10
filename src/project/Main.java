1448ec016b7fb1bce91c18a42ff6fdb6eedc43f5
DmytroTrots <dtrots27@gmail.com>
2021-03-10 13:55:23 +0200

    Init

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,6 +2,7 @@ package project;
 
 public class Main{
     public static void main(String[] args) {
+        System.out.println("Program started");
         new isPalindrome(1, "123454321");
         new isPalindrome(2, "100110011110011001");
         new isPalindrome(3, "123565320");
